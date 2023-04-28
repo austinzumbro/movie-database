41e8c5b930bd3cae2ee79007a29f2ba5164a9a8c
Austin Zumbro <azumbro@gmail.com>
2023-04-28 13:26:53 -0700

    set up database schema and seed

---
+++ db/seeds.sql
@@ -0,0 +1,14 @@
+INSERT INTO movies (movie_name)
+VALUES ("Goldeneye"),
+       ("Titanic"),
+       ("Gone in 60 Seconds"),
+       ("The Notebook"),
+       ("Iron Man 2");
+
+INSERT INTO reviews (movie_id, review)
+VALUES (1, "Pierce Brosnan's first appearance as James Bond."),
+       (2, "What a great movie about a ship sinking."),
+       (2, "I can't believe she threw that necklace in the ocean!"),
+       (3, "They sure got out of there in under a minute!  Wow."),
+       (5, "Tony Stark is back at it again.");
+
